a0ad3f8964685dddb2de907a0b1e62170ebc1160
RafaGdeom <89557826+RafaGdeom@users.noreply.github.com>
2021-08-25 22:03:43 -0300

    ta lá
    Vensemo😎

---
+++ SomoDeDoisNumeros/somodoisnumeros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somodoisnumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double numb1=0;
+            Double numb2=0;
+            Double soma=0;
+            string pergunta="";
+            Console.WriteLine("Digite  primeiro numero ");
+            pergunta = Console.ReadLine();
+            numb1 = Convert.ToDouble(pergunta);
+
+            Console.WriteLine("Digite  segundo numero ");
+            pergunta = Console.ReadLine();
+            numb2 = Convert.ToDouble(pergunta);
+
+            soma = numb1 + numb2;
+            Console.WriteLine($"A soma do numero {numb1} + {numb2} é ={soma}");
+            Console.ReadKey();        
+        }
+    }
+}
